ea9a2e995c9ac03d52b6ae855a5b33b4422c3593
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-04-15 07:00:58 -0700

    finished the code

--- guss the number.py
+++ guss the number.py
@@ -1,19 +1,22 @@
 import random
 generate_number = random.randint(0,20)
 guesses = 0
-
-for i in range(0, 20):
+print(generate_number)
+for i in range(0, 25):
     print("Enter your guess number")
     input_number = input()
     input_number = int(input_number) 
     int(input_number)
-    if input_number >= generate_number:
+    if input_number > 20:
+         print("You must insert the number between 0 and 20")
+    elif input_number > generate_number:
             print("Too High Try Again\n")
             guesses = guesses + 1
-    elif input_number <= generate_number:
+    elif input_number < generate_number:
         print("Too low Try Again\n")
         guesses = guesses + 1
     elif input_number == generate_number:
+        guesses = str(guesses)
+        print("Good job it took you  " + guesses + " guesses" )
         break
-if input_number == generate_number:
-    print("Good job it took you  " + guesses + "guesses" )
+    
