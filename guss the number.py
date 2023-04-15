e5d1303488262e389c862e33fe5222419e1e4af4
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-04-15 06:48:11 -0700

    converting input number to number

--- guss the number.py
+++ guss the number.py
@@ -1,13 +1,15 @@
 import random
 generate_number = random.randint(0,20)
 guesses = 0
+
 for i in range(0, 20):
     print("Enter your guess number")
-    input_number = input() 
+    input_number = input()
+    input_number = int(input_number) 
     int(input_number)
     if input_number >= generate_number:
-        print("Too High Try Again\n")
-        guesses = guesses + 1
+            print("Too High Try Again\n")
+            guesses = guesses + 1
     elif input_number <= generate_number:
         print("Too low Try Again\n")
         guesses = guesses + 1
