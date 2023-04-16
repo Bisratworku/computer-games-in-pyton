6e34c119214dcb0e27104d157b07720a7c33a7a3
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-04-16 01:05:54 -0700

    automation complite

--- fun-fact-generator.py
+++ fun-fact-generator.py
@@ -1,5 +1,11 @@
 fun_facts = ["Ethiopia’s Danakil Depression is the hottest place on Earth.","Ethiopia is home to the lowest place on the African continent, the Danakil Depression.","They run on their own calendar","It was the birthplace of coffee!", "Ethiopian man, Abebe Bikila, was the first black African to win gold at the Olympics!","Addis Ababa is the highest capital city in Africa", "You’ll find lots of vegetarian cuisine in Ethiopia","Ethiopia was never colonised by the Europeans", "Over 80 languages are spoken in the country"] 
 def generate(fact) :
     for i in fact:
-        input("wqe")
+        print("______________________________________________________________________")
+        input("Press Enter to read a fun fact")
+        print("______________________________________________________________________")
         print(i)
+    print("__________________________________________________________________________")
+    print("Think You come again")
+    print("__________________________________________________________________________)")
+generate(fun_facts)
