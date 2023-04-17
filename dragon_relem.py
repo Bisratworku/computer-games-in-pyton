9caf45f05ef7a3c15a89161e7574bb20b01d8cb0
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-04-17 11:16:18 -0700

    project complited

--- dragon_relem.py
+++ dragon_relem.py
@@ -1,31 +1,38 @@
 import random
 import time 
+
 def gen():
-    print("one or two\n")
+    print("You are infront of two huge caves both have dragons inside but one of the dragon is friendly so it will give you tons of gold and the other is evil you need to choose between the cave one and two  \n")
     cave = str(random.randint(1,2))
     answer = input()
+    time.sleep(2)
+    print("you are approching the cave\n")
+    time.sleep(2)
     if cave == answer:
-        print("good play again")
+        print("You got the gold congragulations PRESS ENTER to play again")
         answer = input()
         if answer == "":
             gen()
     else:
-        print("not good try again?")
+        print("YOU died PRESS ENTER try again?")
         answer = input()
         if answer == "":
-            cave_answer()
+            gen()
 
 def cave_answer():
-    print("one or two\n")
+    print("You are infront of two huge caves both have dragons inside but one of the dragon is friendly so it will give you tons of gold and the other is evil you need to choose between the cave one and two  \n")
     cave = str(random.randint(1,2))
     answer = input()
+    time.sleep(2)
+    print("you are approching the cave\n")
+    time.sleep(2)
     if cave == answer:
-        print("good play again")
+        print("You got the gold congragulations PRESS ENTER to play again")
         answer = input()
         if answer == "":
             gen()
     else:
-        print("not good try again?")
+        print("YOU died PRESS ENTER try again?")
         answer = input()
         if answer == "":
             gen()            
